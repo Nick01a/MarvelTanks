8026f4df90e96e5008f864b25ec35c864e3ab23c
kyrychenko <oviotion@gmail.com>
2019-09-01 23:41:40 +0300

    (all mechanics added. Basic game ready)

--- src/play.js
+++ src/play.js
@@ -1,77 +1,7 @@
-const order_player = () => {
-    let modal = document.createElement("div");
-    modal.className = "modal_player";
-    document.body.append(modal);
-    let div = document.createElement("div");
-    div.className = "block1";
-    modal.appendChild(div);
-    // document.getElementsByClassName("play_map")[0].appendChild(modal);
-    let tagDiv = document.createElement("div");
-    tagDiv.className = "player_1";
-    tagDiv.innerHTML = "Player 1";
-    div.appendChild(tagDiv);
-
-    let tagDiv2 = document.createElement("div");
-    tagDiv2.className = "player_2";
-    tagDiv2.innerHTML = "Player 2";
-    div.appendChild(tagDiv2);
-
-
-    let tagDiv3 = document.createElement("div");
-    tagDiv3.className = "heroes";
-    tagDiv3.insertAdjacentHTML('beforeEnd', `<div  class="spiderman avg"></div>`);
-    tagDiv3.insertAdjacentHTML('beforeEnd', `<div  class="ironman avg"></div>`);
-    tagDiv3.insertAdjacentHTML('beforeEnd', `<div  class="hawkeye avg"></div>`);
-    tagDiv3.insertAdjacentHTML('beforeEnd', `<div  class="daredevil avg"></div>`);
-    modal.appendChild(tagDiv3);
-    // modal.insertAdjacentHTML('beforeEnd',`<div class="heroes"></div>` );
-
-
-    // getId('description').insertAdjacentHTML('beforeEnd', `<div style="height:60px;background-color:lightcoral;text-align:center;padding-top:20px; font-weight:bold">Немовля</div>`);
-    let tagDiv4 = document.createElement("button");
-    tagDiv4.className = "start";
-    tagDiv4.innerHTML = "start";
-    tagDiv4.addEventListener("click", generate_map);
-    modal.appendChild(tagDiv4);
-    // modal.insertAdjacentHTML('beforeend', `<div class="start">start</div>`);
-    document.getElementsByClassName("start")[0].addEventListener('click', function () {
-        document.getElementsByClassName("modal_player")[0].style.display = "none";
-    });
-    var counter = 0;
-    // document.getElementById("spiderman").addEventListener('click', function () {
-    //     let div1 = document.createElement("div");
-    //     div1.className = "box";
-    //     div1.insertAdjacentHTML('beforeEnd', `<div id="spiderman" class="spiderman"></div>`);
-    //     arr.push(div1);
-    //     tagDiv.appendChild(arr[0])
-
-    // })
-
-    var hers = document.getElementsByClassName("avg");
-    // console.log(hers.length);
-    for (var i = 0; i < hers.length; i++) {
-        // console.log(hers[i]);
-        hers[i].addEventListener('click', function () {
-            counter++;
-            if (counter % 2 === 1 && counter <= 2) {
-                let div1 = document.createElement("div");
-                div1.id = "player1_hero";
-                div1.className = "box1";
-                // console.log(this);
-                div1.insertAdjacentHTML('beforeEnd', this.outerHTML);
-                tagDiv.appendChild(div1);
-            }
-            else if (counter <= 2) {
-                let div1 = document.createElement("div");
-                div1.id = "player2_hero";
-                div1.className = "box2";
-                // console.log(this);
-                div1.insertAdjacentHTML('beforeEnd', this.outerHTML);
-                tagDiv2.appendChild(div1);
-            }
-        })
-    }
-};
+var direction_1;
+var direction_2;
+var kill_counter_1 = 0;
+var kill_counter_2 = 0;
 
 
 const generate_map = () => {
@@ -91,7 +21,7 @@ const generate_map = () => {
     for (let i = 0; i < 4; i++){
         document.getElementsByClassName("floor")[Math.floor(Math.random() * Math.floor(document.getElementsByClassName("floor").length))].className = "monster";
     }
-
+    setInterval(function(){document.getElementsByClassName("floor")[Math.floor(Math.random() * Math.floor(document.getElementsByClassName("floor").length))].className = "monster";}, 2000);
 };
 
 
@@ -118,23 +48,141 @@ const cords = (item) => {
     }
 };
 
-const fly_fire = (hero,counter) =>{
+
+const start_fire_1 = (hero) => {
+    fly_fire(hero, 1, direction_1);
+    // ultimates(hero, direction_1);
+};
+
+const start_fire_2 = (hero) => {
+    fly_fire(hero, 1, direction_2);
+    // ultimates(hero, direction_2);
+};
+
+
+
+const fly_fire = (hero, counter , dir) =>{
     let hero_cords = cords(hero);
     let game_field = map();
-    if(game_field[hero_cords[0] + counter][hero_cords[1]].className === "floor") {
-        game_field[hero_cords[0] + counter][hero_cords[1]].className = "fire";
-        if (counter > 1)
-            game_field[hero_cords[0] + counter - 1][hero_cords[1]].className = "floor";
-        setTimeout(function() {fly_fire(hero,counter+1)},400);
+    let i;
+    let j;
+    let i_min;
+    let j_min;
+    switch (dir) {
+        case "s":
+        case "ArrowDown":
+            i = counter;
+            j = 0;
+            i_min = counter - 1;
+            j_min = 0;
+            break;
+        case "w":
+        case "ArrowUp":
+            i = -counter;
+            j = 0;
+            i_min = - counter + 1;
+            j_min = 0;
+            break;
+        case "a":
+        case "ArrowLeft":
+            i = 0;
+            j = -counter;
+            i_min = 0;
+            j_min = -counter + 1;
+            break;
+        case "d":
+        case "ArrowRight":
+            i = 0;
+            j = counter;
+            i_min = 0;
+            j_min = counter - 1;
     }
-    if (game_field[hero_cords[0] + counter][hero_cords[1]].className ==="heavy_block")
-        if (counter > 1)
-            game_field[hero_cords[0] + counter - 1][hero_cords[1]].className = "floor";
-    if (game_field[hero_cords[0] + counter][hero_cords[1]].className ==="easy_block"){
-        if (counter > 1)
-            game_field[hero_cords[0] + counter - 1][hero_cords[1]].className = "floor";
-        game_field[hero_cords[0] + counter][hero_cords[1]].className = "floor";}
+            if (game_field[hero_cords[0] + i][hero_cords[1] + j].className === "floor") {
+                game_field[hero_cords[0] + i][hero_cords[1] + j].className = "fire";
+                if (counter > 1)
+                    game_field[hero_cords[0] + i_min][hero_cords[1] + j_min].className = "floor";
+                setTimeout(function () {
+                    fly_fire(hero, counter + 1, dir)
+                }, 200);
+            }
+            if (game_field[hero_cords[0] + i][hero_cords[1] + j].className === "fire")
+                if (counter > 1){
+                    game_field[hero_cords[0] + i_min][hero_cords[1] + j_min].className = "floor";
+                    // game_field[hero_cords[0] + i][hero_cords[1] + j].className = "floor"
+                }
+            if (game_field[hero_cords[0] + i][hero_cords[1] + j].className === "heavy_block")
+                if (counter > 1)
+                    game_field[hero_cords[0] + i_min][hero_cords[1] + j_min].className = "floor";
+            if (game_field[hero_cords[0] + i][hero_cords[1] + j].className === "easy_block") {
+                if (counter > 1)
+                    game_field[hero_cords[0] + i_min][hero_cords[1] + j_min].className = "floor";
+                game_field[hero_cords[0] + i][hero_cords[1] + j].className = "easy_block_damaged";
+                counter = 0;
+            }
+            if (game_field[hero_cords[0] + i][hero_cords[1] + j].className === "monster") {
+                if (counter > 1)
+                    game_field[hero_cords[0] + i_min][hero_cords[1] + j_min].className = "floor";
+                game_field[hero_cords[0] + i][hero_cords[1] + j].className = "floor";
+                if (hero.className === document.getElementById('player1_hero').children.item(0).className){
+                    kill_counter_1 += 1;
+                    console.log(kill_counter_1);
+                }
+                if (hero.className === document.getElementById('player2_hero').children.item(0).className){
+                    kill_counter_2 += 1;
+                    console.log(kill_counter_2);
+                }
+            }
+            if (game_field[hero_cords[0] + i][hero_cords[1] + j].className === "easy_block_damaged") {
+                if (counter > 1)
+                    game_field[hero_cords[0] + i_min][hero_cords[1] + j_min].className = "floor";
+                game_field[hero_cords[0] + i][hero_cords[1] + j].className = "floor";
+            }
+            if (counter >= 1) {
+                if (game_field[hero_cords[0] + i][hero_cords[1] + j].className === document.getElementById('player1_hero').children.item(0).className) {
+                    game_field[hero_cords[0] + i][hero_cords[1] + j].className = "floor";
+                    alert("Player 2 won");
+                }
+            }
+            if (counter >= 1) {
+                if (game_field[hero_cords[0] + i][hero_cords[1] + j].className === document.getElementById('player2_hero').children.item(0).className) {
+                    game_field[hero_cords[0] + i][hero_cords[1] + j].className = "floor";
+                    alert("Player 1 won");
+                }
+            }
 };
+
+
+const ultimates = (hero, dir) => {
+    let hero_cords = cords(hero);
+    let game_field = map();
+    if (hero.className === "ironman avg"){
+        switch (dir) {
+            case "s":
+            case "ArrowDown":
+                i = 1;
+                j = 0;
+                break;
+            case "w":
+            case "ArrowUp":
+                i = -1;
+                j = 0;
+                break;
+            case "a":
+            case "ArrowLeft":
+                i = 0;
+                j = -1;
+                break;
+            case "d":
+            case "ArrowRight":
+                i = 0;
+                j = 1;
+                break;
+        }
+        if (game_field[hero_cords[0] + i][hero_cords[1] + j].className === "floor")
+            game_field[hero_cords[0] + i][hero_cords[1] + j].className = "heavy_block";
+    }
+};
+
 
 const move_u = (swaper) => {
     var c = cords(swaper);
@@ -174,41 +222,60 @@ const move_d = (swaper) => {
 
 
 document.addEventListener("keydown", (event) =>{
+
     switch(event.key){
         case 'w':
             move_u(document.getElementsByClassName(document.getElementById('player1_hero').children.item(0).className).item(0));
+            direction_1 = event.key;
             break;
         case 's':
             move_d(document.getElementsByClassName(document.getElementById('player1_hero').children.item(0).className).item(0));
+            direction_1 = event.key;
             break;
         case 'a':
             move_l(document.getElementsByClassName(document.getElementById('player1_hero').children.item(0).className).item(0));
+            direction_1 = event.key;
             break;
         case 'd':
             move_r(document.getElementsByClassName(document.getElementById('player1_hero').children.item(0).className).item(0));
+            direction_1 = event.key;
             break;
         case ' ':
-            fly_fire(document.getElementsByClassName(document.getElementById('player1_hero').children.item(0).className).item(0), 1);
+            start_fire_1(document.getElementsByClassName(document.getElementById('player1_hero').children.item(0).className).item(0));
+            break;
+        case 'r':
+            ultimates(document.getElementsByClassName(document.getElementById('player1_hero').children.item(0).className).item(0), direction_1);
             break;
         case 'ArrowUp':
             move_u(document.getElementsByClassName(document.getElementById('player2_hero').children.item(0).className).item(0));
+            direction_2 = event.key;
             break;
         case 'ArrowDown':
             move_d(document.getElementsByClassName(document.getElementById('player2_hero').children.item(0).className).item(0));
+            direction_2 = event.key;
             break;
         case 'ArrowLeft':
             move_l(document.getElementsByClassName(document.getElementById('player2_hero').children.item(0).className).item(0));
+            direction_2 = event.key;
             break;
         case 'ArrowRight':
             move_r(document.getElementsByClassName(document.getElementById('player2_hero').children.item(0).className).item(0));
+            direction_2 = event.key;
             break;
         case 'Shift':
-            fly_fire(document.getElementsByClassName(document.getElementById('player2_hero').children.item(0).className).item(0), 1);
+            start_fire_2(document.getElementsByClassName(document.getElementById('player2_hero').children.item(0).className).item(0), 1);
             break;
     }
 });
 
+function include(url ) {
+    var script = document.createElement('script');
+    script.src = url;
+    document.getElementsByTagName('head')[0].appendChild(script);
+}
 
 window.onload = () => {
+    include("./src/start.js");
+    // include("./src/game_over.js");
     order_player();
 };
---
+++ src/start.js
@@ -0,0 +1,70 @@
+let modal = document.createElement("div");
+modal.className = "modal_player";
+document.body.append(modal);
+let tagDiv1 = document.createElement("div");
+tagDiv1.className = "player_1";
+tagDiv1.insertAdjacentHTML('beforeEnd', `<h2> Player 1</h2><hr>`);
+tagDiv1.insertAdjacentHTML('beforeEnd', `<h4> Game keys</h4> `);
+tagDiv1.insertAdjacentHTML('beforeEnd', ` <img class="imageBlock" src="../images/wasd.png" alt=""><br>`);
+tagDiv1.insertAdjacentHTML('beforeEnd', `<h4 style="line-height:0.6"> For shoot</h4> `);
+tagDiv1.insertAdjacentHTML('beforeEnd', ` <img class="shift" src="../images/space.png" alt=""><br>`);
+tagDiv1.insertAdjacentHTML('beforeEnd', `<h4 style="line-height:0.6"> Ulti</h4> `);
+tagDiv1.insertAdjacentHTML('beforeEnd', ` <img class="ulti" src="../images/r.png" alt=""><br>`);
+tagDiv1.insertAdjacentHTML('beforeEnd', `<h4 style="line-height:0.6"> Your hero</h4> `);
+modal.appendChild(tagDiv1);
+
+let tagDiv2 = document.createElement("div");
+tagDiv2.className = "heroes";
+
+tagDiv2.insertAdjacentHTML('beforeEnd', `<div  class="spiderman avg"></div>`);
+tagDiv2.insertAdjacentHTML('beforeEnd', `<div  class="ironman avg" ></div>`);
+tagDiv2.insertAdjacentHTML('beforeEnd', `<div  class="daredevil avg" ></div>`);
+tagDiv2.insertAdjacentHTML('beforeEnd', `<div  class="hawkeye avg" ></div>`);
+modal.appendChild(tagDiv2);
+
+let tagDiv3 = document.createElement("div");
+tagDiv3.className = "player_2";
+tagDiv3.insertAdjacentHTML('beforeEnd', `<h2>Player 2</h2><hr>`);
+tagDiv3.insertAdjacentHTML('beforeEnd', `<h4>Game keys</h4> `);
+tagDiv3.insertAdjacentHTML('beforeEnd', `<img class="imageBlock" src="../images/keyboard-arrows.png" alt=""><br>`);
+tagDiv3.insertAdjacentHTML('beforeEnd', `<h4 style="line-height:0.6"> For shoot</h4> `);
+tagDiv3.insertAdjacentHTML('beforeEnd', `<img class="shift" src="../images/shift.png" alt=""><br>`);
+tagDiv3.insertAdjacentHTML('beforeEnd', `<h4 style="line-height:0.6"> Ulti</h4> `);
+tagDiv3.insertAdjacentHTML('beforeEnd', `<img class="ulti" src="../images/l.png" alt=""><br>`);
+tagDiv3.insertAdjacentHTML('beforeEnd', `<h4 style="line-height:0.6"> Your hero</h4> `);
+modal.appendChild(tagDiv3);
+
+
+let tagDiv4 = document.createElement("button");
+tagDiv4.className = "start";
+tagDiv4.innerHTML = "start";
+tagDiv4.addEventListener("click", generate_map);
+modal.appendChild(tagDiv4);
+document.getElementsByClassName("start")[0].addEventListener('click', function () {
+    document.getElementsByClassName("modal_player")[0].style.display = "none";
+});
+var counter = 0;
+var hers = document.getElementsByClassName("avg");
+for (var i = 0; i < hers.length; i++) {
+    hers[i].addEventListener('click', function () {
+        counter++;
+        if (counter % 2 === 1 && counter <= 2) {
+            let div1 = document.createElement("div");
+            div1.id = "player1_hero";
+            div1.className = "box1";
+            div1.insertAdjacentHTML('beforeEnd', this.outerHTML);
+            tagDiv1.appendChild(div1);
+            this.style.background = 'rgb(128,128,128)';
+        }
+        else if (counter <= 2) {
+            let div1 = document.createElement("div");
+            div1.id = "player2_hero";
+            div1.className = "box2";
+            div1.insertAdjacentHTML('beforeEnd', this.outerHTML);
+            tagDiv3.appendChild(div1);
+            this.style.background = 'rgb(128,128,128)';
+            // this.style.opacity = '0.8';
+        }
+    })
+}
+
